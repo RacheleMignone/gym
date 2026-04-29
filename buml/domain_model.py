af1f1842043d21053ff30ea3598c2a469070ebae
RacheleMignone <57143821+RacheleMignone@users.noreply.github.com>
2026-04-29 09:55:55 +0200

    Initial commit - Generated by BESSER Web Editor

---
+++ buml/domain_model.py
@@ -0,0 +1,23 @@
+####################
+# STRUCTURAL MODEL #
+####################
+
+from besser.BUML.metamodel.structural import (
+    Class, Property, Method, Parameter,
+    BinaryAssociation, Generalization, DomainModel,
+    Enumeration, EnumerationLiteral, Multiplicity,
+    StringType, IntegerType, FloatType, BooleanType,
+    TimeType, DateType, DateTimeType, TimeDeltaType,
+    AnyType, Constraint, AssociationClass, Metadata, MethodImplementationType
+)
+
+# Classes
+
+# Domain Model
+domain_model = DomainModel(
+    name="Class_Diagram",
+    types={},
+    associations={},
+    generalizations={},
+    metadata=None
+)
